f63b860ab7cbfe9217cd22b713383464b6efb21d
PJ <pjloury@gmail.com>
2025-01-05 15:42:28 -0800

    Initial Commit

---
+++ HigherOrLower/HigherOrLowerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HigherOrLowerApp.swift
+//  HigherOrLower
+//
+//  Created by PJ Loury on 1/5/25.
+//
+
+import SwiftUI
+
+@main
+struct HigherOrLowerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
